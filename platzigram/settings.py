7b6ae8103bcc4b77a3a333f6368a8fefb7d259c3
ralfxdev <lopezalexander1019@gmail.com>
2022-11-15 20:08:22 -0600

    platzigram

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'whitenoise.runserver_nostatic',
 
     #Local Apps
 
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    #"whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+#STATIC_ROOT = str(BASE_DIR / 'static')
+#STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     str(BASE_DIR / 'static'),
 ]
